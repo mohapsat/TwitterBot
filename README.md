527f1af012f2251a0ce1e902f0349ae5e3534a70
Satya Mohapatra <mohapsat@gmail.com>
2019-02-18 15:43:14 -0800

    tumidpandora-bot v.01

---
+++ README.md
@@ -0,0 +1,25 @@
+## My Personal Re-tweeter Bot
+
+Hacked together in < 1 hr
+
+###Pre-requisites:
+
+- [x] Signup for a twitter developers account
+- [x] Wait a couple hours or upto a day for approval
+- [x] Create an APP
+
+### Setup
+- [x] Procure API creds from your account
+-[x] Create config.py and set the following env variables
+    - API_KEY
+    - API_SECRET
+    - ACCESS_TOKEN
+    - ACCESS_TOKEN_SECRET
+-[x] in `Main.py` replace users inside users list with the twitter handles of the users you'd like to retweet
+For example:
+`users = ['OpenAI', 'GitHubGameOff', 'SalesforceDevs', 'DeepMindAI', 'GoogleAI', 'techreview']` 
+
+-[x] Schedule to run on a desired interval
+
+- [X] Follow me on twitter [@mohapsat](https://twitter.com/mohapsat)
+- [X] Support Open Source and [Tweepy](https://tweepy.readthedocs.io/en/3.7.0/index.html) 
---
+++ main.py
@@ -0,0 +1,38 @@
+import tweepy
+import conf
+import time
+
+
+auth = tweepy.OAuthHandler(conf.API_KEY, conf.API_SECRET)
+auth.set_access_token(conf.ACCESS_TOKEN, conf.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+### GET id_str for tweeter users to follow
+
+user_ids = [] # empty list to hold ids
+
+users = ['OpenAI', 'GitHubGameOff', 'SalesforceDevs', 'DeepMindAI', 'GoogleAI', 'techreview']
+
+for user in users:
+    u = api.get_user(user)
+    user_ids.append(u.id_str)
+
+# print(user_ids)
+# ['4398626122', '3524101993', '7834512', '4783690002', '33838201', '15808647']
+
+
+### Fetch their latest tweets from the timeline, favorite and retweet
+
+# https://tweepy.readthedocs.io/en/v2.3.0/api.html?highlight=user_timeline#API.user_timeline
+for user_id in user_ids:
+    tweet = api.user_timeline(user_id=user_id, count=1)
+    try:
+        tweet[0].favorite() # favorite the tweet
+        tweet[0].retweet()  # retweet
+    except:
+        print('already retweeted status.')
+    # print(tweet[0].text)
+    time.sleep(2)   # sleep 2 seconds
+
+# extend to do more cool stuff later...
+
